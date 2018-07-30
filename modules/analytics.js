c4f90a1e58dd53fc0c04d3bfe6d8b283a6f0f211
qaws825825 <38966850+qaws825825@users.noreply.github.com>
2018-07-30 14:01:57 +0800

    analyticsSP.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -9,12 +9,15 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 
 //用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
 //格式是 exports.骰組檔案名字.function名
+
 function parseInput(rplyToken, inputStr) {
 	//console.log('InputStr: ' + inputStr);
 	_isNaN = function(obj) 	{
 	return isNaN(parseInt(obj));  
 	}
 	
+	var stringSP = ('null');
+	var countSP = 0;
 	let msgSplitor = (/\S+/ig);	
 	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
@@ -90,7 +93,22 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/運氣|運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
 	
 
+		\//鴨霸獸指令開始於此
+	if (trigger.match(/鴨霸獸|巴獸/) != null) return exports.funny.randomReply() ;	
+	if (trigger.match(/運氣|運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣	
 	
+	if (trigger != stringSP) {
+		stringSP == trigger;
+		countSP = 0;
+	}
+	else{ if (countSP > 3){
+		return string;
+		}
+	     	else{
+		 countSP++;
+		}
+	}
+		
 	/*tarot 指令
 	if (trigger.match(/猜拳/) != null) {
 		return RockPaperScissors(inputStr, mainMsg[1]);
