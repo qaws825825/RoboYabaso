edb21e30f7b6ac0837853276edbc1978a94c1af8
samSA0562 <39234565+samSA0562@users.noreply.github.com>
2018-08-03 23:27:10 +0800

    Update SA_Script.js

--- roll/SA_Script.js
+++ roll/SA_Script.js
@@ -1,6 +1,29 @@
 var rply ={type : 'text'}; //type是必需的,但可以更改
+var ReplyCount = 0; //跟上回話用全域變數_整數
+var ReplyString;    //跟上回話用全域變數_字串
 
-
+function IsNeedReply(trigger)
+{
+	if (trigger != ReplyString)
+	{
+		//console.log(trigger);
+		//console.log(ReplyString);
+		ReplyString = trigger;
+		ReplyCount = 1;
+	}
+	else
+	{
+		//console.log(ReplyCount);
+		ReplyCount++;
+		if (ReplyCount >= 3) 
+		{
+			ReplyCount = 0;
+			return true;
+		}
+			
+	}
+	return false;
+}
 //回話
 function ReplyMsg(trigger) {
 	rply.text = trigger;
@@ -8,5 +31,6 @@ return rply;
 }
 
 module.exports = {
+	IsNeedReply,
 	ReplyMsg	
 };
