91bac2c6084fb82f5a46f663f1424636e03e8436
samSA0562 <39234565+samSA0562@users.noreply.github.com>
2018-12-12 16:20:24 +0800

    Update and rename replyMsgToLine.js to MsgToLine.js

--- modules/MsgToLine.js
+++ modules/MsgToLine.js
@@ -3,10 +3,8 @@ function replyMsgToLine(rplyToken, rplyVal, options) {
 	let rplyObj = {
 	replyToken: rplyToken,
 	messages: [rplyVal]
-	
 	}
 	let rplyJson = JSON.stringify(rplyObj); 
-	console.log(rplyJson);
 	var request = https.request(options, function(response) {
 //	console.log('Status: ' + response.statusCode);
 //	console.log('Headers: ' + JSON.stringify(response.headers));
@@ -21,8 +19,28 @@ function replyMsgToLine(rplyToken, rplyVal, options) {
 	request.end(rplyJson);
 }
 
+function sendMsgToLine(sendToken, sendVal, options) {
+	let sendObj = {
+	to: sendToken,
+	messages: [sendVal]
+	}
+	let sendJson = JSON.stringify(sendObj); 
+	var request = https.request(options, function(response) {
+//	console.log('Status: ' + response.statusCode);
+//	console.log('Headers: ' + JSON.stringify(response.headers));
+	response.setEncoding('utf8');
+	response.on('data', function(body) {
+//		console.log(body); 
+	});
+	});
+	request.on('error', function(e) {
+	console.log('Request error: ' + e.message);
+	})
+	request.end(sendJson);
+}
 
 module.exports = {
-	replyMsgToLine:replyMsgToLine
+	replyMsgToLine,
+	sendMsgToLine
 };
 
